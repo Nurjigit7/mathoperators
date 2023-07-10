5fc458bed8fd4317b9df6519280d0c25c772aa14
626 <nurjigitumarov22@gmail.com>
2023-07-10 19:03:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,136 @@
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+          Person person = new Person("Nurjigit", "Umarov", 22, 968402278);
+         System.out.println(person);
+        // System.out.println("Hello world!");
+        //  int a=5;
+        //a=a+5; increment bul bir sandyn 1koboiuusu a++;
+        //a=a+1;
+      /*  a++;
+        a+=5;
+        System.out.println(a);*/
+     /*   double a= Math.sqrt(25); //Tamyrdan chygaruu
+        double b= Math.pow(5,2);  //Birinchi berilgen sandy ekinchi berilgen sanga darajaga kotoruu
+        double c=Math.round(5.5);  //Okruglenie butun sandy karai
+        double s=Math.ceil(7.1);    //Жогоруга которот
+        double d=Math.floor(8.9);  //ыллдыйга карай тегеректейт
+        int max=Math.max(4,3); //макс максималный
+        int min =Math.min(1000,1001);*/ //минималный число
+
+      /*  Scanner scanner=new Scanner(System.in);
+        double a=scanner.nextDouble();
+        double e=Math.floor(a);
+        System.out.println(a-e);*/
+      /* Random ran=new Random();
+        int a=ran.nextInt(1,10);
+
+        System.out.println(a);*/
+
+     /*   Scanner sc =new Scanner(System.in);
+        String a= "hello world";
+        String  b="java";
+        System.out.println(a+b);*/
+
+     /*   Scanner scanner=new Scanner(System.in);
+       int a =scanner.nextInt();
+       int b=scanner.nextInt();
+       int c=scanner.nextInt();
+       int hours = a + 60  + 60 ;
+       int s = b + 60;
+       int summa = hours + s + c;
+        System.out.println(summa);*/
+
+
+
+     /*  Random random=new Random();
+        int a= random.nextInt(1,10);
+        int b= random.nextInt(1,10);
+        int c= random.nextInt(1,10);
+        System.out.println(a+" " +b+ " " +c);
+        System.out.println((a+b+c)/3);*/
+
+      /*  Random random=new Random();
+        int a= random.nextInt(100,200);
+        int b= random.nextInt(100,200);
+        int c= random.nextInt(100,200);
+        System.out.println(a+"  " +b+"  " +c);
+        System.out.println(a+b+c);*/
+     /*   Scanner sc=new Scanner(System.in);
+        System.out.println("atynyz kim");
+        String g=sc.nextLine();
+
+        System.out.println("Sizdin jashynyz");
+        int j= sc.nextInt();
+        int joop=2021-j;*/
+        // Random random=new Random();
+
+        //   int c= random.nextInt(-51,2);
+        // System.out.println(c); //"eki sandyn randon sanyn chygary"
+     /*   Scanner scanner=new Scanner(System.in);
+        int g=scanner.nextInt();
+        System.out.println(g%2);*/
+//      Scanner sc=new Scanner(System.in);
+//        int a =sc.nextInt();
+//        int b =sc.nextInt();
+//        int c =sc.nextInt();
+//
+//        int house = a * 60 * 60;
+//        int minute =b *60;
+//        int sum =house + minute +c;
+//        System.out.println(sum);
+        //Тапшырма уч убакыттагы санды чыгаруу
+
+//        Scanner sc =new Scanner(System.in);
+//        int r=sc.nextInt();
+//        int y=r/100;
+//        int u=r%100/10;
+//        int m=r % 10;
+//        System.out.println(" "+y+","+u+","+m);
+        //уч oрунду  санды 3ко болу жолуу менен чыгаруу
+//        Scanner sc = new Scanner(System.in);
+//        int r= 68;
+//        int e = 74;
+//        int j = 46;
+//        int s= 97;
+//        int o=Math.max(r,e);
+//        int f=Math.max(j,s);
+//        int l=Math.max(o,f);
+//        int g=Math.min(r,e);
+//        int m=Math.min(s,j);
+//        int x=Math.min(g,m);
+//        System.out.println(l);
+//        System.out.println(x);
+//        Random random = new Random();
+//        int b= random.nextInt(-51,2);
+//
+//        System.out.println(b);
+//       Random random =new Random();
+//            int san = random.nextInt(1,100);
+//            for (int i = 0; i <100 ; i++) {
+//                if(i%10==7){
+//                    System.out.println(i);
+//                }
+//
+//        }
+//        Random random=new Random();
+//        int[]array=new int[10];
+//        for (int i = 0; i < array.length ; i++) {
+//            array[i]= random.nextInt(1,200);
+//        }
+//
+//        System.out.println(Arrays.toString(array(array)));
+//    }static int[] array(int[]arr) {
+//        int []array=new int[10];
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i]%2!=0) {
+//                array[i] = arr[i];
+//            }
+//        }
+//        return array;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,58 @@
+public class Person {
+    private String name;
+    private String lastName;
+    private int age;
+    private int phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String name, String lastName, int age, int phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
